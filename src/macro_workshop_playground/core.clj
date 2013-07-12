fe79965a3d4b3235f932d70033a62e861082bd24
Dave Newton <davelnewton@gmail.com>
2013-07-12 10:14:38 -0500

    Working on macro workshop exercises.

--- src/macro_workshop_playground/core.clj
+++ src/macro_workshop_playground/core.clj
@@ -7,21 +7,35 @@
 ;;;;
 ;;;; Funhouse
 ;;;;
+
+(def opmap '{ + -, - +, * /, / * })
+
  
 (defn funhouse [forms] 
-  ...)
+  (let [sym (first forms)]
+    (conj (rest forms) (opmap (first forms)))))
 
+(defn funhouse [expr]
+  (if (list? expr)
+    (map funhouse expr)
+    (or ('{ + -, - +, * /, / * } expr) expr)))
 
 ;;;;
 ;;;; Macros
 ;;;;
  
 (defmacro in-funhouse [forms] 
-  ...)
+  (funhouse forms))
 
 (defmacro do-funhouse [& forms] 
-  ...)
+  (list 'map 'funhouse (list 'quote forms)))
 
+(defmacro do-funhouse [& forms] 
+  (list 'do
+    (list 'quote (map funhouse forms))))
+
+(defmacro do-funhouse [& forms] 
+  (cons 'do (map funhouse forms)))
 
 ;;;;
 ;;;; Syntax-quote
@@ -30,6 +44,21 @@
 (defmacro debug [expr]
   ...)
 
+(defmacro debug [expr]
+  (let [i# expr]
+  (list 'do
+    (list 'println
+      (list 'quote expr) :> i#)
+    i#)))
+
+(defmacro debug-2 [expr]
+  `(do (println (list ~@expr))
+    ~expr))
+
+(defmacro debug-3 [expr]
+  `(let [val# ~expr]
+    (do (println '~expr :> val#)
+      val#)))
 
 ;;;;
 ;;;; Multiple execution
@@ -39,16 +68,41 @@
   ...)
 
 
+(defmacro and-1 [expr1 expr2]
+  `(let [val1# ~expr1]
+    (if val1#
+      ~expr2
+      val1#)))
+
 ;;;;
 ;;;; Using functions in macros
 ;;;;
 
 (defn stringify-static [parts]
-  ...)
+  (map
+    (fn [part]
+      (if
+        (or (keyword? part)
+            (string? part)
+            (number? part))
+        (str part)
+        part))
+    parts))
+
+(defn constant? (some-fun keyword? string? number?))
+
+(defn stringify-static [parts]
+  (map
+    (fn [part]
+      (if (constant? part)
+        (str part)
+        part))
+    parts))
+
+; TODO Merge adjacent strings
 
 (defmacro pre-str [& parts]
-  ...)
-
+  `(str ~@(stringify-static parts)))
 
 ;;;;
 ;;;; Recursive macros
